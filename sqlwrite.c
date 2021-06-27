24b0ac0807a8ea47791e16e15cdfe18a780b0841
Adrian Kennard <a@k.gg>
2021-06-27 10:04:11 +0100

    Test enum

--- sqlwrite.c
+++ sqlwrite.c
@@ -36,134 +36,132 @@ SQL_RES *res;
 SQL_ROW row = 0;
 SQL_FIELD *field;
 
-void
-fail (char *e)
+void fail(char *e)
 {
-   fprintf (stderr, "Error: %s [%s]\n", sql_error (&sql), e);
-   sql_close (&sql);
-   exit (-1);
+   fprintf(stderr, "Error: %s [%s]\n", sql_error(&sql), e);
+   sql_close(&sql);
+   exit(-1);
 }
 
-int
-main (int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
    int quiet = 0;
    int showdiff = 0;
    int onlylisted = 0;
    const char *sqlhost = NULL,
-      *sqldatabase = NULL,
-      *sqltable = NULL,
-      *sqluser = NULL,
-      *sqlpass = NULL,
-      *sqlconf = NULL;
+       *sqldatabase = NULL,
+       *sqltable = NULL,
+       *sqluser = NULL,
+       *sqlpass = NULL,
+       *sqlconf = NULL;
    unsigned int sqlport = 0;
    int n,
-     f;
+    f;
    char *e,
-     s;
+    s;
    sql_string_t query = { };
    sql_string_t where = { };
 
    poptContext popt;            // context for parsing command-line options
    const struct poptOption optionsTable[] = {
-      {"sql-conf", 0, POPT_ARGFLAG_SHOW_DEFAULT | POPT_ARG_STRING, &sqlconf, 0, "Client config file", "filename"},
-      {"sql-host", 'h', POPT_ARG_STRING, &sqlhost, 0, "SQL server host",
-       "hostname/ip"},
-      {"sql-port", 0, POPT_ARG_INT, &sqlport, 0, "SQL server port",
-       "port"},
-      {"sql-user", 'u', POPT_ARG_STRING, &sqluser, 0, "SQL username",
-       "username"},
-      {"sql-pass", 'p', POPT_ARG_STRING, &sqlpass, 0, "SQL password",
-       "password"},
-      {"sql-database", 'd', POPT_ARG_STRING, &sqldatabase, 0, "SQL database",
-       "database"},
-      {"sql-table", 't', POPT_ARG_STRING, &sqltable, 0, "SQL table", "table"},
-      {"show-diff", 's', POPT_ARG_NONE, &showdiff, 0, "Show diff, don't update",
-       0},
-      {"only-listed", 'o', POPT_ARG_NONE, &onlylisted, 0, "Only updated fields listed on command line", 0},
-      {"quiet", 'q', POPT_ARG_NONE, &quiet, 0, "Quiet", 0},
-      {"debug", 'v', POPT_ARG_NONE, &sqldebug, 0, "Debug", 0},
-      POPT_AUTOHELP {NULL, 0, 0, NULL, 0}
+      { "sql-conf", 0, POPT_ARGFLAG_SHOW_DEFAULT | POPT_ARG_STRING, &sqlconf, 0, "Client config file", "filename" },
+      { "sql-host", 'h', POPT_ARG_STRING, &sqlhost, 0, "SQL server host",
+       "hostname/ip" },
+      { "sql-port", 0, POPT_ARG_INT, &sqlport, 0, "SQL server port",
+       "port" },
+      { "sql-user", 'u', POPT_ARG_STRING, &sqluser, 0, "SQL username",
+       "username" },
+      { "sql-pass", 'p', POPT_ARG_STRING, &sqlpass, 0, "SQL password",
+       "password" },
+      { "sql-database", 'd', POPT_ARG_STRING, &sqldatabase, 0, "SQL database",
+       "database" },
+      { "sql-table", 't', POPT_ARG_STRING, &sqltable, 0, "SQL table", "table" },
+      { "show-diff", 's', POPT_ARG_NONE, &showdiff, 0, "Show diff, don't update",
+       0 },
+      { "only-listed", 'o', POPT_ARG_NONE, &onlylisted, 0, "Only updated fields listed on command line", 0 },
+      { "quiet", 'q', POPT_ARG_NONE, &quiet, 0, "Quiet", 0 },
+      { "debug", 'v', POPT_ARG_NONE, &sqldebug, 0, "Debug", 0 },
+      POPT_AUTOHELP { NULL, 0, 0, NULL, 0 }
    };
 
-   popt = poptGetContext (NULL, argc, argv, optionsTable, 0);
-   poptSetOtherOptionHelp (popt, "[database] [table] {name[=[value]/$var/@file]}");
+   popt = poptGetContext(NULL, argc, argv, optionsTable, 0);
+   poptSetOtherOptionHelp(popt, "[database] [table] {name[=[value]/$var/@file]}");
    {
-      int c = poptGetNextOpt (popt);
+      int c = poptGetNextOpt(popt);
       if (c < -1)
-         errx (1, "%s: %s\n", poptBadOption (popt, POPT_BADOPTION_NOALIAS), poptStrerror (c));
+         errx(1, "%s: %s\n", poptBadOption(popt, POPT_BADOPTION_NOALIAS), poptStrerror(c));
    }
 
-   if (!sqldatabase && poptPeekArg (popt))
-      sqldatabase = poptGetArg (popt);
-   if (!sqltable && poptPeekArg (popt))
-      sqltable = poptGetArg (popt);
+   if (!sqldatabase && poptPeekArg(popt))
+      sqldatabase = poptGetArg(popt);
+   if (!sqltable && poptPeekArg(popt))
+      sqltable = poptGetArg(popt);
    if (!sqldatabase || !sqltable)
    {
-      poptPrintUsage (popt, stderr, 0);
+      poptPrintUsage(popt, stderr, 0);
       return 2;
    }
 
-   char **values = malloc (argc * sizeof (*values));
+   char **values = malloc(argc * sizeof(*values));
    int valuen = 0;
-   while (valuen < argc && (values[valuen] = (char *) poptGetArg (popt)))
+   while (valuen < argc && (values[valuen] = (char *) poptGetArg(popt)))
       valuen++;
 
    // updating local environment variables based on command line, only the "default" entries need later processing
    for (n = 0; n < valuen; n++)
    {
       char *p = values[n];
-      while (*p && (isalnum (*p) || *p == '_' || *p == '-'))
+      while (*p && (isalnum(*p) || *p == '_' || *p == '-'))
          p++;
       if (*p == '=')
       {
-         putenv (values[n]);
+         putenv(values[n]);
       } else if (*p == '$')
       {                         // defined by reference to environment variable of another name...
          *p = 0;
-         setenv (values[n], getenv (p + 1), 1);
+         setenv(values[n], getenv(p + 1), 1);
          *p = '$';
       } else if (*p == '@')
       {                         // defined as reference to contents of a file
-         int f = open (p + 1, O_RDONLY);
+         int f = open(p + 1, O_RDONLY);
          if (f < 0)
-            err (1, "%s", p + 1);
+            err(1, "%s", p + 1);
          struct stat s = { };
-         fstat (f, &s);
-         if (!fstat (f, &s))
+         fstat(f, &s);
+         if (!fstat(f, &s))
          {
-            char *buf = malloc (s.st_size + 1);
-            if (read (f, buf, s.st_size) != s.st_size)
-               errx (1, "Bad read %s", p + 1);
+            char *buf = malloc(s.st_size + 1);
+            if (read(f, buf, s.st_size) != s.st_size)
+               errx(1, "Bad read %s", p + 1);
             buf[s.st_size] = 0;
             *p = 0;
-            setenv (values[n], buf, 1);
+            setenv(values[n], buf, 1);
             *p = '@';
-            free (buf);
+            free(buf);
          }
-         close (f);
+         close(f);
       } else if (!onlylisted)
-         unsetenv (values[n]);  // using default even if exists, so not taken from environment
+         unsetenv(values[n]);   // using default even if exists, so not taken from environment
    }
 
    // connect to database
-   sql_real_connect (&sql, sqlhost, sqluser, sqlpass, sqldatabase, sqlport, NULL, 0, 1, sqlconf);
-   sql_transaction (&sql);
+   sql_real_connect(&sql, sqlhost, sqluser, sqlpass, sqldatabase, sqlport, NULL, 0, 1, sqlconf);
+   sql_transaction(&sql);
    // construct a WHERE line based on a unique key for which we have values
-   res = sql_safe_query_store_free (&sql, sql_printf ("SHOW INDEX FROM `%#S`", sqltable));
+   res = sql_safe_query_store_free(&sql, sql_printf("SHOW INDEX FROM `%#S`", sqltable));
    {
       char key[257],
-        ok = 0;
+       ok = 0;
       *key = 0;
-      while ((row = sql_fetch_row (res)))
+      while ((row = sql_fetch_row(res)))
       {
-         e = getenv (row[4]);
-         if (strcmp (row[2], key))
+         e = getenv(row[4]);
+         if (strcmp(row[2], key))
          {                      // start new key
             if (ok)
                break;           // found a key already
-            strcpy (key, row[2]);
-            sql_free_s (&where);
+            strcpy(key, row[2]);
+            sql_free_s(&where);
             ok = 1;
          }
          if (*row[1] == '1')
@@ -173,161 +171,147 @@ main (int argc, const char *argv[])
          else
          {                      // unique key
             if (!where.query)
-               sql_sprintf (&where, "WHERE ");
+               sql_sprintf(&where, "WHERE ");
             else
-               sql_sprintf (&where, " AND ");
-            sql_sprintf (&where, "`%#S`=", row[4]);
-            if (isdigit (e[0]) && isdigit (e[1]) && e[2] == '/'
-                && isdigit (e[3]) && isdigit (e[4]) && e[5] == '/'
-                && isdigit (e[6]) && isdigit (e[7]) && isdigit (e[8])
-                && isdigit (e[9]) && (!e[10]
-                                      || (isspace (e[10]) && isdigit (e[11])
-                                          && isdigit (e[12]) && e[13] == ':'
-                                          && isdigit (e[14])
-                                          && isdigit (e[15]) && e[16] == ':' && isdigit (e[17]) && isdigit (e[18]) && !e[19])))
+               sql_sprintf(&where, " AND ");
+            sql_sprintf(&where, "`%#S`=", row[4]);
+            if (isdigit(e[0]) && isdigit(e[1]) && e[2] == '/'
+                && isdigit(e[3]) && isdigit(e[4]) && e[5] == '/' && isdigit(e[6]) && isdigit(e[7]) && isdigit(e[8]) && isdigit(e[9]) && (!e[10] || (isspace(e[10]) && isdigit(e[11]) && isdigit(e[12]) && e[13] == ':' && isdigit(e[14]) && isdigit(e[15]) && e[16] == ':' && isdigit(e[17]) && isdigit(e[18]) && !e[19])))
             {                   // funny date format
-               sql_sprintf (&where, "'%.4s-%.2s-%.2s", e + 6, e + 3, e + 0);
-               while (*e && (isdigit (*e) || *e == ':' || *e == ' '))
-                  sql_sprintf (&where, "%c", *e++);
-               sql_sprintf (&where, "'");
+               sql_sprintf(&where, "'%.4s-%.2s-%.2s", e + 6, e + 3, e + 0);
+               while (*e && (isdigit(*e) || *e == ':' || *e == ' '))
+                  sql_sprintf(&where, "%c", *e++);
+               sql_sprintf(&where, "'");
             } else
-               sql_sprintf (&where, "%#s", e);
+               sql_sprintf(&where, "%#s", e);
          }
       }
       if (!ok)
-         sql_free_s (&where);
+         sql_free_s(&where);
    }
-   sql_free_result (res);
+   sql_free_result(res);
    if (!where.query)
-      return fprintf (stderr, "Could not find unique key set for which we have data\n");
-   res = sql_safe_query_store_free (&sql, sql_printf ("SELECT * FROM `%#S` %s", sqltable, where.query));
-   row = sql_fetch_row (res);
-   field = sql_fetch_field (res);
-   sql_free_s (&query);
+      return fprintf(stderr, "Could not find unique key set for which we have data\n");
+   res = sql_safe_query_store_free(&sql, sql_printf("SELECT * FROM `%#S` %s", sqltable, where.query));
+   row = sql_fetch_row(res);
+   field = sql_fetch_field(res);
+   sql_free_s(&query);
    if (row)
    {                            // UPDATE
-      sql_sprintf (&query, "UPDATE `%#S` SET", sqltable);
+      sql_sprintf(&query, "UPDATE `%#S` SET", sqltable);
       s = ' ';
    } else
    {                            // INSERT
-      sql_sprintf (&query, "INSERT INTO `%#S` ", sqltable);
+      sql_sprintf(&query, "INSERT INTO `%#S` ", sqltable);
       s = '(';
-      for (f = 0; f < sql_num_fields (res); f++)
+      for (f = 0; f < sql_num_fields(res); f++)
       {
-         for (n = 0; n < valuen && strcmp (values[n], field[f].name); n++);
-         if (n < valuen || (!onlylisted && getenv (field[f].name)))
+         for (n = 0; n < valuen && strcmp(values[n], field[f].name); n++);
+         if (n < valuen || (!onlylisted && getenv(field[f].name)))
          {
-            sql_sprintf (&query, "%c`%#S`", s, field[f].name);
+            sql_sprintf(&query, "%c`%#S`", s, field[f].name);
             s = ',';
          }
       }
-      sql_sprintf (&query, ") VALUES ");
+      sql_sprintf(&query, ") VALUES ");
       s = '(';
    }
    // values
-   for (f = 0; f < sql_num_fields (res); f++)
+   for (f = 0; f < sql_num_fields(res); f++)
    {
-      for (n = 0; n < valuen && strcmp (values[n], field[f].name); n++);
-      e = getenv (field[f].name);
+      for (n = 0; n < valuen && strcmp(values[n], field[f].name); n++);
+      e = getenv(field[f].name);
       if (!e && onlylisted)
          continue;
       if (n < valuen || (!onlylisted && e))
       {
-         void showfield (char *p)
-         {
+         void showfield(char *p) {
             if (!p)
             {
-               printf ("<i>NULL</i>");
+               printf("<i>NULL</i>");
                return;
             }
             while (*p)
             {
                if (*p == '<')
-                  printf ("&lt;");
+                  printf("&lt;");
                else if (*p == '>')
-                  printf ("&gt;");
+                  printf("&gt;");
                else if (*p == '&')
-                  printf ("&amp;");
+                  printf("&amp;");
                else if (*p < ' ')
-                  putchar (' ');
+                  putchar(' ');
                else
-                  putchar (*p);
+                  putchar(*p);
                p++;
             }
          }
          if (!e && n < valuen)
             e = field[f].def;
-	 if(field[f].type == FIELD_TYPE_ENUM)
-		 for(char *p=e;*p;p++)if(*p=='\t')*p=',';
-         sql_sprintf (&query, "%c", s);
+         if (field[f].type == FIELD_TYPE_ENUM || (field[f].flags & ENUM_FLAG))
+            for (char *p = e; *p; p++)
+               if (*p == '\t')
+                  *p = ',';
+         sql_sprintf(&query, "%c", s);
          s = ',';
          if (row)
          {
-            sql_sprintf (&query, "`%#S`=", field[f].name);
-            if (showdiff && strcmp (e ? : "", row[f] ? : ""))
+            sql_sprintf(&query, "`%#S`=", field[f].name);
+            if (showdiff && strcmp(e ? : "", row[f] ? : ""))
             {
-               if (!
-                   ((!e || !*e) && (!row[f] || !strncmp (row[f], "0000", 4))
-                    && (field[f].type == FIELD_TYPE_DATE || field[f].type == MYSQL_TYPE_NEWDATE
-                        || field[f].type == FIELD_TYPE_TIMESTAMP || field[f].type == FIELD_TYPE_DATETIME)))
+               if (!((!e || !*e) && (!row[f] || !strncmp(row[f], "0000", 4)) && (field[f].type == FIELD_TYPE_DATE || field[f].type == MYSQL_TYPE_NEWDATE || field[f].type == FIELD_TYPE_TIMESTAMP || field[f].type == FIELD_TYPE_DATETIME)))
                {
                   if (showdiff++ == 1)
-                     printf ("Change");
-                  printf (" %s=", field[f].name);
-                  showfield (row[f]);
-                  printf ("→");
-                  showfield (e);
+                     printf("Change");
+                  printf(" %s=", field[f].name);
+                  showfield(row[f]);
+                  printf("→");
+                  showfield(e);
                }
             }
          } else if (showdiff)
          {
             if (showdiff++ == 1)
-               printf ("New");
-            printf (" %s=", field[f].name);
-            showfield (e);
+               printf("New");
+            printf(" %s=", field[f].name);
+            showfield(e);
          }
          if (!e)
          {
-            sql_sprintf (&query, "NULL");
+            sql_sprintf(&query, "NULL");
          } else
          {
             if ((field[f].type == FIELD_TYPE_DATE || field[f].type == MYSQL_TYPE_NEWDATE
                  || field[f].type == FIELD_TYPE_TIMESTAMP
-                 || field[f].type == FIELD_TYPE_DATETIME) && isdigit (e[0])
-                && isdigit (e[1]) && e[2] == '/' && isdigit (e[3])
-                && isdigit (e[4]) && e[5] == '/' && isdigit (e[6])
-                && isdigit (e[7]) && isdigit (e[8]) && isdigit (e[9])
-                && (!e[10]
-                    || (isspace (e[10]) && isdigit (e[11])
-                        && isdigit (e[12]) && e[13] == ':'
-                        && isdigit (e[14]) && isdigit (e[15]) && e[16] == ':' && isdigit (e[17]) && isdigit (e[18]) && !e[19])))
+                 || field[f].type == FIELD_TYPE_DATETIME) && isdigit(e[0])
+                && isdigit(e[1]) && e[2] == '/' && isdigit(e[3])
+                && isdigit(e[4]) && e[5] == '/' && isdigit(e[6]) && isdigit(e[7]) && isdigit(e[8]) && isdigit(e[9]) && (!e[10] || (isspace(e[10]) && isdigit(e[11]) && isdigit(e[12]) && e[13] == ':' && isdigit(e[14]) && isdigit(e[15]) && e[16] == ':' && isdigit(e[17]) && isdigit(e[18]) && !e[19])))
             {                   // funny date format
-               sql_sprintf (&query, "'%.4s-%.2s-%.2s", e + 6, e + 3, e + 0);
-               while (*e && (isdigit (*e) || *e == ':' || *e == ' '))
-                  sql_sprintf (&query, "%c", *e++);
-               sql_sprintf (&query, "'");
-            } else if (!*e && (field[f].type == FIELD_TYPE_DATE || field[f].type == MYSQL_TYPE_NEWDATE
-                               || field[f].type == FIELD_TYPE_TIMESTAMP || field[f].type == FIELD_TYPE_DATETIME))
-               sql_sprintf (&query, "%#s", "0000-00-00");
+               sql_sprintf(&query, "'%.4s-%.2s-%.2s", e + 6, e + 3, e + 0);
+               while (*e && (isdigit(*e) || *e == ':' || *e == ' '))
+                  sql_sprintf(&query, "%c", *e++);
+               sql_sprintf(&query, "'");
+            } else if (!*e && (field[f].type == FIELD_TYPE_DATE || field[f].type == MYSQL_TYPE_NEWDATE || field[f].type == FIELD_TYPE_TIMESTAMP || field[f].type == FIELD_TYPE_DATETIME))
+               sql_sprintf(&query, "%#s", "0000-00-00");
             else
-               sql_sprintf (&query, "%#s", e);
+               sql_sprintf(&query, "%#s", e);
          }
       }
    }
    if (!row)
-      sql_sprintf (&query, ")");
+      sql_sprintf(&query, ")");
    else
-      sql_sprintf (&query, " %s", where.query);
-   sql_free_result (res);
+      sql_sprintf(&query, " %s", where.query);
+   sql_free_result(res);
    if (s == ',' && !showdiff)
-      sql_safe_query_s (&sql, &query);
+      sql_safe_query_s(&sql, &query);
    if (!quiet)
    {
-      int id = sql_insert_id (&sql);
+      int id = sql_insert_id(&sql);
       if (id)
-         printf ("%d", id);
+         printf("%d", id);
    }
-   sql_safe_commit (&sql);
-   sql_close (&sql);
+   sql_safe_commit(&sql);
+   sql_close(&sql);
    return 0;
 }
