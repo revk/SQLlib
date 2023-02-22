329e0594de6b454af249cba67c643ab8d67b676d
RevK <github@k.gg>
2023-02-22 07:39:19 +0000

    FFS

--- sqllib.c
+++ sqllib.c
@@ -311,7 +311,7 @@ size_t sql_len_s(sql_s_t * q)
 
 void sql_open_s(sql_s_t * q)
 {                               // Open (or continue) a string
-   if (!q || !q->f)
+   if (!q || q->f)
       return;                   // already open, or not valid
    sql_free_s(q);
    q->f = open_memstream(&q->string, &q->len);
